8855320f3d20133e06bb633fd768f67ed367b244
RooterLee <jaehunc.l@gmail.com>
2017-06-03 15:45:47 +0900

    test

--- app/src/main/java/scapp/com/res_server/com/howmanybooksdoyouread/MainActivity.java
+++ app/src/main/java/scapp/com/res_server/com/howmanybooksdoyouread/MainActivity.java
@@ -1,12 +1,10 @@
 package scapp.com.res_server.com.howmanybooksdoyouread;
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 
 import android.content.Intent;
 import android.widget.Toast;
@@ -26,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 scanIntegrator.initiateScan();
             }
         });
